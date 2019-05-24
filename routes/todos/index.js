62804511eefd59a696743f3817bd95ef28161839
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-05-24 15:21:21 +0100

    Create the REST endpoints

--- routes/todos/index.js
+++ routes/todos/index.js
@@ -1,14 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-var Todo = require("../models/todos");
-
-router.get("/", function(req, res) {
-  Todo.find(function(err, todos) {
-    if (err) return console.error(err);
-    res.render("todos", { title: "Todos", todos: todos });
-  });
-});
+var Todo = require("../../models/todos");
 
 router.get("/", function(req, res, next) {
   Todo.findAsync()
