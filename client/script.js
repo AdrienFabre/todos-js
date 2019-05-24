6da9bff652cc7482fef885bb7f6e4514635b21fa
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-05-24 16:18:59 +0100

    Add AJAX CRUD requests

--- client/script.js
+++ client/script.js
@@ -2,11 +2,15 @@ var todoTemplate = require("../views/partials/todo.hbs");
 
 var $ = require("jquery");
 
-$(function() {
-  $("input").on("click", function() {
-    $(this)
-      .parent()
-      .toggleClass("checked");
+$("ul").on("change", "li :checkbox", function() {
+  var $this = $(this),
+    $input = $this[0],
+    $li = $this.parent(),
+    id = $li.attr("id"),
+    checked = $input.checked,
+    data = { done: checked };
+  updateTodo(id, data, function(d) {
+    $this.next().toggleClass("checked");
   });
 });
 
@@ -20,6 +24,41 @@ $(function() {
       e.stopPropagation();
       return false;
     }
+  });
+});
+
+$("ul").on("keydown", "li span", function(e) {
+  var $this = $(this),
+    $span = $this[0],
+    $li = $this.parent(),
+    id = $li.attr("id"),
+    key = e.keyCode,
+    target = e.target,
+    text = $span.innerHTML,
+    data = { text: text };
+  $this.addClass("editing");
+  if (key === 27) {
+    //escape key
+    $this.removeClass("editing");
+    document.execCommand("undo");
+    target.blur();
+  } else if (key === 13) {
+    //enter key
+    updateTodo(id, data, function(d) {
+      $this.removeClass("editing");
+      target.blur();
+    });
+    e.preventDefault();
+  }
+});
+
+$("ul").on("click", "li a", function() {
+  var $this = $(this),
+    $input = $this[0],
+    $li = $this.parent(),
+    id = $li.attr("id");
+  deleteTodo(id, function(e) {
+    deleteTodoLi($li);
   });
 });
 
@@ -39,4 +78,34 @@ var addTodo = function() {
       $("#add-todo-text").val("");
     }
   });
+};
+
+var updateTodo = function(id, data, cb) {
+  $.ajax({
+    url: "/api/todos/" + id,
+    type: "PUT",
+    data: data,
+    dataType: "json",
+    success: function(data) {
+      cb();
+    }
+  });
+};
+
+var deleteTodo = function(id, cb) {
+  $.ajax({
+    url: "/api/todos/" + id,
+    type: "DELETE",
+    data: {
+      id: id
+    },
+    dataType: "json",
+    success: function(data) {
+      cb();
+    }
+  });
+};
+
+var deleteTodoLi = function($li) {
+  $li.remove();
 };
